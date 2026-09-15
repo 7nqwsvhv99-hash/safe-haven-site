b2f776a702ee53f6d667259e10a75f79e5ac35e2
7nqwsvhv99-hash <7nqwsvhv99@privaterelay.appleid.com>
2026-09-14 20:05:45 -0500

    Link new volunteer application

--- src/app/volunteer/page.tsx
+++ src/app/volunteer/page.tsx
@@ -183,7 +183,7 @@ export default function VolunteerPage() {
             Join our team of dedicated volunteers and help animals in our community.
           </p>
           <Button asChild size="lg">
-            <a href="https://safehavenforpets.org/apply-to-volunteer/" target="_blank" rel="noreferrer">Complete the Volunteer Application</a>
+            <Link href="/volunteer-application">Complete the Volunteer Application</Link>
           </Button>
         </div>
       </section>
