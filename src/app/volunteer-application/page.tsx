38f6ee2ded367312c5186a02a93496c40063eaee
7nqwsvhv99-hash <7nqwsvhv99@privaterelay.appleid.com>
2026-09-22 11:38:31 -0500

    Add general clinic volunteer option

--- src/app/volunteer-application/page.tsx
+++ src/app/volunteer-application/page.tsx
@@ -33,6 +33,7 @@ const interestOptions = [
   "Social Media & Content",
   "Administrative Support",
   "Fundraising & Event Support",
+  "Clinic Team - General Volunteer",
   "Clinic Team – Instrument Sterilization",
   "Clinic Team – Front Room",
   "Clinic Team – Back Room Documentation",
