da850d59600028d25d2f59a31e010fd8bcbe9676
7nqwsvhv99-hash <7nqwsvhv99@privaterelay.appleid.com>
2026-09-21 19:10:20 -0500

    Protect team portal routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, request) => {
+  if (request.nextUrl.pathname.startsWith("/portal")) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
